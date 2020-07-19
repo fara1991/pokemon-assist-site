2dfa2a4ca2c1ada2112b5afd0e7daa09178f0202
Fara <game_Fara@outlook.com>
2020-07-20 06:18:13 +0900

    https-portal追加

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,11 +3,8 @@
 cd `dirname $0`
 . ../.env
 
-docker-compose up -d --build 
 docker-compose exec -T workspace bash -c '
     php artisan migrate:rollback &&
     php artisan migrate &&
-    php artisan db:seed &&
-    npm run prod
+    php artisan db:seed
 '
-
--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -3,10 +3,5 @@
 cd `dirname $0`
 . ../.env
 
-docker-compose exec -T workspace bash -c '
-    php artisan migrate:rollback &&
-    php artisan migrate &&
-    php artisan db:seed
-'
 docker-compose exec workspace bash
 
